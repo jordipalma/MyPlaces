d92726834bd6bee2ca9d743bdbaaf7fb519b3e1c
jordi.palma@gmail.com <jordi.palma@gmail.com>
2018-09-20 13:33:19 +0200

    Initial Commit

---
+++ MyPlaces/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyPlaces
+//
+//  Created by Jordi Palma Sanchez on 20/9/18.
+//  Copyright © 2018 Exploracat. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MyPlaces/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyPlaces
+//
+//  Created by Jordi Palma Sanchez on 20/9/18.
+//  Copyright © 2018 Exploracat. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
